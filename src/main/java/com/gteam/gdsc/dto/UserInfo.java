d5b4f82fdec1cd70f5f00d4843a4db03f1aa969a
kons2003 <mj030313@naver.com>
2024-01-07 06:07:43 +0900

    UserInfo Swagger Api 설명 추가

--- src/main/java/com/gteam/gdsc/dto/UserInfo.java
+++ src/main/java/com/gteam/gdsc/dto/UserInfo.java
@@ -1,12 +1,16 @@
 package com.gteam.gdsc.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.time.LocalDate;
 
 @Data
 public class UserInfo {
+    @ApiModelProperty(value = "생년월일")
     private LocalDate birthday; // 생년월일
+    @ApiModelProperty(value = "나이")
     private Integer age; // 나이
+    @ApiModelProperty(value = "성별")
     private String gender; // 성별
 }
