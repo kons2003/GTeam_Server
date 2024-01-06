096a109141ed6cf4dbd79dac78445a47c53e5e28
kons2003 <mj030313@naver.com>
2024-01-07 06:03:50 +0900

    Swagger Api controller 테스트 코드 작성

--- src/main/java/com/gteam/gdsc/controller/TestController.java
+++ src/main/java/com/gteam/gdsc/controller/TestController.java
@@ -2,6 +2,7 @@ package com.gteam.gdsc.controller;
 
 import com.gteam.gdsc.domain.User;
 import com.gteam.gdsc.service.AuthService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class TestController {
     private final AuthService authService;
 
     @GetMapping("/test")
+    @ApiOperation(value = "테스트", notes = "테스트 중입니다.")
     public User test(Principal principal) {
         return authService.test(principal);
     }
