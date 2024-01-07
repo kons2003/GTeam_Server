c69003540068a492859c678c4fe09f6ff25c4f82
kons2003 <mj030313@naver.com>
2024-01-07 11:45:29 +0900

    ApiOperation 어노테이션 삭제

--- src/main/java/com/gteam/gdsc/controller/TestController.java
+++ src/main/java/com/gteam/gdsc/controller/TestController.java
@@ -2,7 +2,6 @@ package com.gteam.gdsc.controller;
 
 import com.gteam.gdsc.domain.User;
 import com.gteam.gdsc.service.AuthService;
-import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
