73e718779ee0b82c44cda3522a9152bb10e2d959
kons2003 <mj030313@naver.com>
2024-01-07 18:05:44 +0900

    SwaggerConfig 코드 추가

--- src/main/java/com/gteam/gdsc/config/SwaggerConfig.java
+++ src/main/java/com/gteam/gdsc/config/SwaggerConfig.java
@@ -1,4 +1,25 @@
 package com.gteam.gdsc.config;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("neat fin API")
+                .description("neat fin 프로젝트를 위한 API")
+                .version("1.0.0");
+    }
 }
