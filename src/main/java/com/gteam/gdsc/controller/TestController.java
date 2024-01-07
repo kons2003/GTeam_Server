716f73c1bee8561edbd5ee5377ca91d52459284c
kons2003 <mj030313@naver.com>
2024-01-07 18:27:14 +0900

    TestController Swagger 어노테이션 추가

--- src/main/java/com/gteam/gdsc/controller/TestController.java
+++ src/main/java/com/gteam/gdsc/controller/TestController.java
@@ -2,17 +2,23 @@ package com.gteam.gdsc.controller;
 
 import com.gteam.gdsc.domain.User;
 import com.gteam.gdsc.service.AuthService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 
+@Tag(name = "test API", description = "Swagger 테스트 컨트롤러")
 @RestController
 @RequiredArgsConstructor
 public class TestController {
     private final AuthService authService;
 
+    @Operation(summary = "구글 로그인", description = "test 메소드 실행")
+    @Parameter(name = "principal", description = "test")
     @GetMapping("/test")
     public User test(Principal principal) {
         return authService.test(principal);
