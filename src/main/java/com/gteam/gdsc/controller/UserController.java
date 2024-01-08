85318d46b83796db463baff45f139c42ca149aad
kons2003 <mj030313@naver.com>
2024-01-08 17:20:11 +0900

    UserController Create, Read, update 추가

--- src/main/java/com/gteam/gdsc/controller/UserController.java
+++ src/main/java/com/gteam/gdsc/controller/UserController.java
@@ -2,30 +2,44 @@ package com.gteam.gdsc.controller;
 
 import com.gteam.gdsc.domain.User;
 import com.gteam.gdsc.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
-/*@RestController
+@RestController
+@Tag(name = "UserController")
 @RequestMapping("/user")
 @RequiredArgsConstructor
 public class UserController {
     private UserService userService;
 
     // CREATE
+    @Operation(description = "신규 회원 정보 추가 입력")
     @PostMapping("/new")
-    public String userInfo(@RequestBody User user) {
-        return userService.createUser(user);
-    }*/
+    public ResponseEntity<String> userInfo(@Parameter @RequestBody User user) {
+        return ResponseEntity.ok(userService.addUserInformation(user));
+    }
 
     // READ
-   /* @GetMapping("/{id}")
-    public*/
+    @Operation(description = "회원 조회")
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUser(@Parameter @PathVariable("id") String id) {
+        return ResponseEntity.ok(userService.findByUserId);
+    }
 
     // UPDATE
+    @Operation(description = "회원 정보 수정")
+    @PutMapping("/{id}/update")
+    public ResponseEntity<User> updateUser(@Parameter @PathVariable("id") String id) {
+        return ResponseEntity.ok(userService.findByUserId);
+    }
 
     // DELETE
 
-//}
+}
