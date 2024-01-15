3ab430295864e4f08233d3373bc59d847e396804
kons2003 <mj030313@naver.com>
2024-01-16 05:30:01 +0900

    주석 추가

--- src/main/java/com/gteam/gdsc/dto/Token.java
+++ src/main/java/com/gteam/gdsc/dto/Token.java
@@ -10,5 +10,5 @@ import lombok.Data;
 @AllArgsConstructor
 public class Token {
     @SerializedName("access_token")
-    private String accessToken;
+    private String accessToken; // // 애플리케이션이 Google API 요청을 승인하기 위해 보내는 토큰
 }
