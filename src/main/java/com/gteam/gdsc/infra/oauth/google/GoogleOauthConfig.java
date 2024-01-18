cab757a05c9c68530f3149bacb178d0c6f9c02b6
kons2003 <mj030313@naver.com>
2024-01-18 14:17:46 +0900

    GoogleOauthConfig 환경변수 코드 불러오도록 함

--- src/main/java/com/gteam/gdsc/infra/oauth/google/GoogleOauthConfig.java
+++ src/main/java/com/gteam/gdsc/infra/oauth/google/GoogleOauthConfig.java
@@ -1,12 +1,26 @@
 package com.gteam.gdsc.infra.oauth.google;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
 
-@ConfigurationProperties(prefix = "spring.security.oauth2.client.registration.google")
-public record GoogleOauthConfig(
-        String redirectUri,
-        String clientId,
-        String clientSecret,
-        String[] scope
-) {
+public class GoogleOauthConfig {
+
+    private String googleRedirctUri;
+    private String googleClientId;
+    private String googleClientSecret;
+    private String googleScope;
+
+    public GoogleOauthConfig(
+            @Value("${GOOGLE_REDIRECT_URI}")
+            String googleRedirctUri,
+            @Value("${GOOGLE_CLIENT_ID}")
+            String googleClientId,
+            @Value("${GOOGLE_CLIENT_SECRET}")
+            String googleClientSecret,
+            @Value("${GOOGLE_SCOPE}")
+            String googleScope) {
+        this.googleRedirctUri = googleRedirctUri;
+        this.googleClientId = googleClientId;
+        this.googleClientSecret = googleClientSecret;
+        this.googleScope = googleScope;
+    }
 }
