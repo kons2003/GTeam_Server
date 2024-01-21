6ab79d83815a3d655a214e3452e117da85d6505f
kons2003 <mj030313@naver.com>
2024-01-21 11:53:54 +0900

    User 클래스 생성

--- src/main/java/com/gteam/gdsc/domain/User.java
+++ src/main/java/com/gteam/gdsc/domain/User.java
@@ -1,12 +1,10 @@
 package com.gteam.gdsc.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import java.time.LocalDate;
 
 @Entity
 @Getter
@@ -14,33 +12,5 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 public class User {
-    @Id
-    @Column(name = "USER_ID")
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
 
-    @Column(name = "USER_EMAIL", nullable = false)
-    private String email;
-
-    @Column(name = "USER_NAME", nullable = false)
-    private String name;
-
-    @Column(name = "USER_PICTURE_URL", nullable = false)
-    private String pictureUrl;
-
-    @Column(name = "USER_TOKEN", columnDefinition = "TEXT")
-    private String token;
-
-    @Enumerated(EnumType.STRING)
-    @Column(name = "USER_ROLE", nullable = false)
-    private Role role;
-
-/*    @Column(name = "USER_BIRTHDAY", nullable = false)
-    private LocalDate birthday;
-
-    @Column(name = "USER_AGE", nullable = false)
-    private Integer age;
-
-    @Column(name = "USER_GENDER", nullable = false)
-    private String gender;*/
 }
