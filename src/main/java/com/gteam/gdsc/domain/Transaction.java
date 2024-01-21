597c059784b4aec4c491687be56d1614b51fd593
kons2003 <mj030313@naver.com>
2024-01-21 11:55:37 +0900

    Transaction 클래스 생성

---
+++ src/main/java/com/gteam/gdsc/domain/Transaction.java
@@ -0,0 +1,15 @@
+package com.gteam.gdsc.domain;
+
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Transaction {
+}
