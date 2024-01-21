3241cde44505c143a8f00328b1b8d07b5e8e64f4
kons2003 <mj030313@naver.com>
2024-01-22 07:06:54 +0900

    AccountInfo 데이터 추가

--- src/main/java/com/gteam/gdsc/dto/AccountInfo.java
+++ src/main/java/com/gteam/gdsc/dto/AccountInfo.java
@@ -4,4 +4,8 @@ import lombok.Data;
 
 @Data
 public class AccountInfo {
+    private Long accountId; // account id
+    private String bankName; // 은행명
+    private String accountName; // 계좌명
+    private Long balance; // 잔고
 }
