24ccafc423df373429da3889622638dfabf6e4be
kons2003 <mj030313@naver.com>
2024-01-22 12:05:01 +0900

    TransactionInfo 코드 정리

--- src/main/java/com/gteam/gdsc/dto/TransactionInfo.java
+++ src/main/java/com/gteam/gdsc/dto/TransactionInfo.java
@@ -9,12 +9,14 @@ public class TransactionInfo {
     private Long transactionId; // transaction id
     private LocalDateTime useDateTime; // 사용 날짜, 시간
     private String transactionType; // 구분(수입, 지출 등)
+
     private String depositDestination; // 입금처
-    private String withdrawalDestination; // 출금처
+    private String incomeCategory; // 소득 분야
     private Long incomeAmount; // 소득 금액
     private Long monthlyIncome; // 한 달 소득 금액
+
+    private String withdrawalDestination; // 출금처
+    private String expenditureCategory; // 지출 분야
     private Long expenditureAmount; // 지출 금액
     private Long monthlyExpenditure; // 한 달 지출 금액
-    private String incomeCategory; // 소득 분야
-    private String expenditureCategory; // 지출 분야
 }
