e3ea0ad2e7bffe12957142af7ce13032870a68c5
kons2003 <mj030313@naver.com>
2024-01-22 12:01:49 +0900

    Account 코드 추가

--- src/main/java/com/gteam/gdsc/domain/Account.java
+++ src/main/java/com/gteam/gdsc/domain/Account.java
@@ -1,6 +1,6 @@
 package com.gteam.gdsc.domain;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -12,4 +12,21 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Account {
+    
+    @Id
+    @Column(name = "ACCOUNT_ID")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(name = "BANK_NAME", nullable = false)
+    private String bankName; // 은행명
+    
+    @Column(name = "ACCOUNT_NAME", nullable = false)
+    private String accountName; // 계좌명
+    
+    @Column(name = "BALANCE", nullable = false)
+    private String balance; // 잔고
+    
+    
+
 }
