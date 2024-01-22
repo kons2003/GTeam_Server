2b13be69f3a5152857f881095ad518dba49ebe2c
kons2003 <mj030313@naver.com>
2024-01-22 14:48:20 +0900

    createAccount 메서드 추가

--- src/main/java/com/gteam/gdsc/service/AccountService.java
+++ src/main/java/com/gteam/gdsc/service/AccountService.java
@@ -1,7 +1,31 @@
 package com.gteam.gdsc.service;
 
+import com.gteam.gdsc.domain.Account;
+import com.gteam.gdsc.dto.AccountInfo;
+import com.gteam.gdsc.repository.AccountRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@RequiredArgsConstructor
 public class AccountService {
+
+    private final AccountRepository accountRepository;
+
+    // 계좌 추가
+    @Transactional
+    public String createAccount(AccountInfo accountInfo) {
+        Account account = Account.builder()
+                .bankName(accountInfo.getBankName())
+                .accountName(accountInfo.getAccountName())
+                .balance(accountInfo.getBalance())
+                .build();
+        accountRepository.save(account);
+        return "account 추가 성공";
+    }
+
+
+
+
 }
