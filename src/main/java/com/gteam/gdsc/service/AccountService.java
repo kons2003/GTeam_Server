0e0c26dce9654629578743239aff0ccebbd37b2f
kons2003 <mj030313@naver.com>
2024-01-25 10:23:44 +0900

    findAccountByName -> readAccountByName으로 변경

--- src/main/java/com/gteam/gdsc/service/AccountService.java
+++ src/main/java/com/gteam/gdsc/service/AccountService.java
@@ -9,12 +9,12 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class AccountService {
 
     private final AccountRepository accountRepository;
 
     // 계좌 추가
-    @Transactional
     public String createAccount(AccountInfo accountInfo) {
         Account account = Account.builder()
                 .bankName(accountInfo.getBankName())
@@ -26,13 +26,12 @@ public class AccountService {
     }
 
     // 계좌 불러오기
-    public Account findAccountByName(String name) {
-        return accountRepository.findAccountByName(name)
+    public Account readAccountByName(String accountName) {
+        return accountRepository.findAccountByAccountName(accountName)
                 .orElseThrow(() -> new IllegalArgumentException("잘못된 계좌명 입니다."));
     }
 
     // 계좌 수정하기
-    @Transactional
     public String updateAccount(AccountInfo accountInfo) {
         Account account = accountRepository.findById(accountInfo.getAccountId())
                 .orElseThrow(() -> new IllegalArgumentException("잘못된 계좌 ID 입니다."));
@@ -46,9 +45,8 @@ public class AccountService {
     }
 
     // 계좌 삭제
-    @Transactional
     public String deleteAccount(AccountInfo accountInfo) {
-        Account account = findAccountByName(accountInfo.getAccountName());
+        Account account = readAccountByName(accountInfo.getAccountName());
         accountRepository.delete(account);
         return "삭제 성공";
     }
