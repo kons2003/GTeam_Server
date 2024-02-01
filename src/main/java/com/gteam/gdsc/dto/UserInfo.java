bf19f7f8607e90eee48f6c32807a156eba00939f
kons2003 <mj030313@naver.com>
2024-02-01 21:50:08 +0900

    UserInfo 코드 추가

--- src/main/java/com/gteam/gdsc/dto/UserInfo.java
+++ src/main/java/com/gteam/gdsc/dto/UserInfo.java
@@ -1,12 +1,22 @@
 package com.gteam.gdsc.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 @Data
+@Builder
 public class UserInfo {
+    private Long id;
+    private String email; // 이메일
+    private Boolean verifiedEmail; // 검증된 이메일
+    private String name; // 이름
+    private String pictureUrl; // 프로필 이미지
+    private String locale; // 국가
     private LocalDate birthday; // 생년월일
-    private Integer age; // 나이
     private String gender; // 성별
+    private List<TransactionInfo> transactions = new ArrayList<>();
 }
