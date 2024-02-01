8bdaff4b01675a259af34e7f8782a1efcfeede9d
kons2003 <mj030313@naver.com>
2024-02-01 21:50:16 +0900

    UserRepository 코드 추가

--- src/main/java/com/gteam/gdsc/repository/UserRepository.java
+++ src/main/java/com/gteam/gdsc/repository/UserRepository.java
@@ -1,12 +1,14 @@
 package com.gteam.gdsc.repository;
 
-import com.gteam.gdsc.auth.domain.GoogleUser;
+import com.gteam.gdsc.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<GoogleUser, Long> {
-    Optional<GoogleUser> findByEmail(String userEmail);
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserById(Long userId);
+    Optional<User> findByEmail(String userEmail);
+    Optional<User> findUserByName(String userName);
 }
