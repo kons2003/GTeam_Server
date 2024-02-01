88595853d2245d4a5fe6fbd09555c717c3c13161
kons2003 <mj030313@naver.com>
2024-02-01 21:46:53 +0900

    Account 컨트롤러 추가

---
+++ src/main/java/com/gteam/gdsc/controller/AccountController.java
@@ -0,0 +1,40 @@
+package com.gteam.gdsc.controller;
+
+import com.gteam.gdsc.dto.AccountInfo;
+import com.gteam.gdsc.dto.UserInfo;
+import com.gteam.gdsc.service.AccountService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("account")
+@RequiredArgsConstructor
+public class AccountController {
+    private final AccountService accountService;
+
+    @PostMapping("/new")
+    @Operation(description = "신규 계좌 정보 추가")
+    public ResponseEntity<String> createAccount(@RequestBody AccountInfo accountInfo) {
+        return ResponseEntity.ok(accountService.createAccount(accountInfo));
+    }
+
+    @GetMapping("/{name}")
+    @Operation(description = "계좌 조회")
+    public ResponseEntity<AccountInfo> findAccountByName(@PathVariable("name") String name) {
+        return ResponseEntity.ok(accountService.findAccountByName(name).toAccountInfo());
+    }
+
+    @PutMapping
+    @Operation(description = "계좌 정보 수정")
+    public ResponseEntity<String> updateAccount(@RequestBody AccountInfo accountInfo) {
+        return ResponseEntity.ok(accountService.updateAccount(accountInfo));
+    }
+
+    @DeleteMapping
+    @Operation(description = "계좌 정보 삭제")
+    public ResponseEntity<String> deleteAccount(@RequestBody AccountInfo accountNameInfo) {
+        return ResponseEntity.ok(accountService.deleteAccount(accountNameInfo));
+    }
+}
