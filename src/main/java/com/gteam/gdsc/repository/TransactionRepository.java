238f4cc9ec18ccd1d4de66ca87655d857870f2a1
kons2003 <mj030313@naver.com>
2024-02-01 21:49:06 +0900

    TransactionRepository 코드 추가

--- src/main/java/com/gteam/gdsc/repository/TransactionRepository.java
+++ src/main/java/com/gteam/gdsc/repository/TransactionRepository.java
@@ -3,5 +3,8 @@ package com.gteam.gdsc.repository;
 import com.gteam.gdsc.domain.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+    Optional<Transaction> findTransactionByTransactionName(String transactionName);
 }
