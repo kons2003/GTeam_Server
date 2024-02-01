88fd6116cf9986b7c3365a00fd1531bc895acdef
kons2003 <mj030313@naver.com>
2024-02-01 21:47:13 +0900

    AccountInfo 코드 추가

--- src/main/java/com/gteam/gdsc/dto/AccountInfo.java
+++ src/main/java/com/gteam/gdsc/dto/AccountInfo.java
@@ -1,11 +1,17 @@
 package com.gteam.gdsc.dto;
 
+import lombok.Builder;
 import lombok.Data;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Data
+@Builder
 public class AccountInfo {
-    private Long accountId; // account id
+    private Long id; // account id
     private String bankName; // 은행명
     private String accountName; // 계좌명
     private Long balance; // 잔고
+    private List<TransactionInfo> transactions = new ArrayList<>();
 }
