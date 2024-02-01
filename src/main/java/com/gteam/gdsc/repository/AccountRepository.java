654ba7720258d7bfe8f1993174b6552c7770d1b6
kons2003 <mj030313@naver.com>
2024-02-01 21:47:25 +0900

    AccountRepository 코드 추가

--- src/main/java/com/gteam/gdsc/repository/AccountRepository.java
+++ src/main/java/com/gteam/gdsc/repository/AccountRepository.java
@@ -1,6 +1,7 @@
 package com.gteam.gdsc.repository;
 
 import com.gteam.gdsc.domain.Account;
+import com.gteam.gdsc.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,6 @@ import java.util.Optional;
 
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
+    Optional<Account> findAccountById(Long accountId);
     Optional<Account> findAccountByAccountName(String accountName);
 }
