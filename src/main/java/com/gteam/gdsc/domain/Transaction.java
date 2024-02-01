1d445d2dccebad5361f37ecc482b4ee708cea61b
kons2003 <mj030313@naver.com>
2024-02-01 21:48:23 +0900

    Transaction 처리

--- src/main/java/com/gteam/gdsc/domain/Transaction.java
+++ src/main/java/com/gteam/gdsc/domain/Transaction.java
@@ -1,5 +1,6 @@
 package com.gteam.gdsc.domain;
 
+import com.gteam.gdsc.dto.TransactionInfo;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -19,6 +20,9 @@ public class Transaction {
     @Column(name = "TRANSACTION_ID")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id; // transaction id
+
+    @Column(name = "TRANSACTION_NAME")
+    private String transactionName; // 거래명
 
     @Column(name = "USE_DATETIME", nullable = false)
     private LocalDateTime useDateTime; // 사용 날짜, 시간
@@ -49,4 +53,130 @@ public class Transaction {
 
     @Column(name = "MONTHLY_EXPENDITURE")
     private Long monthlyExpenditure; // 한 달 지출 금액
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "USER_ID")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "ACCOUNT_ID")
+    private Account account;
+
+    public TransactionInfo toUserInfo() {
+        if (user != null) {
+            return TransactionInfo.builder()
+                    .transactionId(this.id)
+                    .transactionName(this.transactionName)
+                    .useDateTime(this.useDateTime)
+                    .useType(this.useType)
+                    .depositDestination(this.depositDestination)
+                    .incomeCategory(this.incomeCategory)
+                    .incomeAmount(this.incomeAmount)
+                    .monthlyIncome(this.monthlyIncome)
+                    .withdrawalDestination(this.withdrawalDestination)
+                    .expenditureCategory(this.expenditureCategory)
+                    .expenditureAmount(this.expenditureAmount)
+                    .monthlyExpenditure(this.monthlyExpenditure)
+                    .userName(user.getName())
+                    .build();
+        }
+        return TransactionInfo.builder()
+                .transactionId(this.id)
+                .transactionName(this.transactionName)
+                .useDateTime(this.useDateTime)
+                .useType(this.useType)
+                .depositDestination(this.depositDestination)
+                .incomeCategory(this.incomeCategory)
+                .incomeAmount(this.incomeAmount)
+                .monthlyIncome(this.monthlyIncome)
+                .withdrawalDestination(this.withdrawalDestination)
+                .expenditureCategory(this.expenditureCategory)
+                .expenditureAmount(this.expenditureAmount)
+                .monthlyExpenditure(this.monthlyExpenditure)
+                .build();
+    }
+
+    public TransactionInfo toAccountInfo() {
+        if (account != null) {
+            return TransactionInfo.builder()
+                    .transactionId(this.id)
+                    .transactionName(this.transactionName)
+                    .useDateTime(this.useDateTime)
+                    .useType(this.useType)
+                    .depositDestination(this.depositDestination)
+                    .incomeCategory(this.incomeCategory)
+                    .incomeAmount(this.incomeAmount)
+                    .monthlyIncome(this.monthlyIncome)
+                    .withdrawalDestination(this.withdrawalDestination)
+                    .expenditureCategory(this.expenditureCategory)
+                    .expenditureAmount(this.expenditureAmount)
+                    .monthlyExpenditure(this.monthlyExpenditure)
+                    .userName(account.getAccountName())
+                    .build();
+        }
+        return TransactionInfo.builder()
+                .transactionId(this.id)
+                .transactionName(this.transactionName)
+                .useDateTime(this.useDateTime)
+                .useType(this.useType)
+                .depositDestination(this.depositDestination)
+                .incomeCategory(this.incomeCategory)
+                .incomeAmount(this.incomeAmount)
+                .monthlyIncome(this.monthlyIncome)
+                .withdrawalDestination(this.withdrawalDestination)
+                .expenditureCategory(this.expenditureCategory)
+                .expenditureAmount(this.expenditureAmount)
+                .monthlyExpenditure(this.monthlyExpenditure)
+                .build();
+    }
+
+    public void userUpdate(Transaction transaction) {
+        this.id = transaction.id;
+        this.transactionName = transaction.transactionName;
+        this.useDateTime = transaction.useDateTime;
+        this.useType = transaction.useType;
+        this.depositDestination = transaction.depositDestination;
+        this.incomeCategory = transaction.incomeCategory;
+        this.incomeAmount = transaction.incomeAmount;
+        this.monthlyIncome = transaction.monthlyIncome;
+        this.withdrawalDestination = transaction.withdrawalDestination;
+        this.expenditureCategory = transaction.expenditureCategory;
+        this.expenditureAmount = transaction.expenditureAmount;
+        this.monthlyExpenditure = transaction.monthlyExpenditure;
+        if (user != null) {
+            changeUser(transaction.user);
+            return;
+        }
+        this.user = transaction.user;
+    }
+
+    public void accountUpdate(Transaction transaction) {
+        this.id = transaction.id;
+        this.transactionName = transaction.transactionName;
+        this.useDateTime = transaction.useDateTime;
+        this.useType = transaction.useType;
+        this.depositDestination = transaction.depositDestination;
+        this.incomeCategory = transaction.incomeCategory;
+        this.incomeAmount = transaction.incomeAmount;
+        this.monthlyIncome = transaction.monthlyIncome;
+        this.withdrawalDestination = transaction.withdrawalDestination;
+        this.expenditureCategory = transaction.expenditureCategory;
+        this.expenditureAmount = transaction.expenditureAmount;
+        this.monthlyExpenditure = transaction.monthlyExpenditure;
+        if (account != null) {
+            changeAccount(transaction.account);
+            return;
+        }
+        this.account = transaction.account;
+    }
+
+    public void changeUser(User user) {
+        this.user = user;
+        user.getTransactions().add(this);
+    }
+
+    public void changeAccount(Account account) {
+        this.account = account;
+        account.getTransactions().add(this);
+    }
 }
