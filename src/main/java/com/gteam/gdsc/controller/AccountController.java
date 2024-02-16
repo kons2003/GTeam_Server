5c60afbbb3ff26207ded22643bc17a6056367dba
kons2003 <mj030313@naver.com>
2024-02-16 22:52:06 +0900

    RequestMapping 어노테이션 수정

--- src/main/java/com/gteam/gdsc/controller/AccountController.java
+++ src/main/java/com/gteam/gdsc/controller/AccountController.java
@@ -9,7 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("account")
+@RequestMapping("/account")
 @RequiredArgsConstructor
 public class AccountController {
     private final AccountService accountService;
